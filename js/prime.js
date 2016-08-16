dd00aa4c86893f85eed46eb38642b5c44055e2f8
Jackson Meyer and Timothy Herold <timothy@>
2016-08-16 13:48:04 -0700

    will add workinf js to return the prime numbers

--- js/prime.js
+++ js/prime.js
@@ -1,11 +1,29 @@
-$(document).ready(function()  {
-  $("form").submit(function(event) {
-    var num = $("#input").val();
-    debugger;
-    for (i = num; i > 0; i--) {
-      debugger;
-      num = num * i;
-      console.log(num);
+var getPrimes = function(input) {
+  var numbers = [];
+  for (i = 2; i <= input; i++) {
+    numbers.push(i);
+  }
+
+  numbers.forEach(function(number) {
+    for (i = number + number; i <= input; i += number) {
+      if (numbers.indexOf(i) >= 0) {
+        numbers.splice(numbers.indexOf(i), 1);
+      }
     }
+  });
+  return numbers;
+}
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    var userInput = parseInt($("input").val());
+
+    var primes = getPrimes(userInput);
+    $("ul").children().remove();
+    primes.forEach(function(prime) {
+      $("ul").append("<li>" + prime + "</li>");
     });
+
+    event.preventDefault();
+  });
 });
